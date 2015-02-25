6e714c510acf8c958bd4a3c6aebab1fb7a183fc2
Douglas Hewitt <doughewitt4@gmail.com>
2015-02-24 17:45:00 -0800

    Completed Loops

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -12,7 +12,22 @@
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
     /* WORK HERE */
-    return @"";
+    NSMutableString *output = [NSMutableString stringWithString:@""];
+    if (number < otherNumber) {
+        while (number <= otherNumber) {
+            NSString *smallNumber = [NSString stringWithFormat:@"%ld", (long)number];
+            [output appendString:smallNumber];
+            number++;
+        }
+    } else {
+        for (NSString *smallNumber = [NSString stringWithFormat:@"%ld", (long)otherNumber]; otherNumber <= number; otherNumber++) {
+            smallNumber = [NSString stringWithFormat:@"%ld", (long)otherNumber];
+            [output appendString:smallNumber];
+        }
+    }
+  
+    
+    return output;
 }
 
 @end
